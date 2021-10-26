27423f86575fe50a3deacd0fc394bdbabd7a894b
emrullah04 <emrullahisik@vedas.com.tr>
2021-10-26 16:58:11 +0300

    Updates and additions have been made. Update is DbContext, Anket, Duyurular, Etkinlikler, HavaDurumu... Add is OdaDetayFormu, OdaRezevFormu, ToplantiNotVeSonucBildirgeleri

---
+++ VedasPortal/Data/VedasDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VedasPortal.Data
+{
+    public class VedasDbContext : IdentityDbContext
+    {
+        public VedasDbContext(DbContextOptions<VedasDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- VedasPortal/Startup.cs
+++ VedasPortal/Startup.cs
@@ -31,11 +31,11 @@ namespace VedasPortal
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<VedasDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<VedasDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
