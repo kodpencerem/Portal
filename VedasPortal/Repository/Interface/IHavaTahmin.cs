80cdbc158785927cecf34f731edb8f16234e238c
emrullah04 <emrullahisik@vedas.com.tr>
2021-11-17 11:53:48 +0300

    Add IHavaTahmin Interface

---
+++ VedasPortal/Repository/Interface/IHavaTahmin.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using VedasPortal.Models.OpenWeatherMapApi;
+
+namespace VedasPortal.Repository.Interface
+{
+    public interface IHavaTahmin
+    {
+        Task<HavaTahmini[]> HavaTahminiGetir(params string[] sehirler);
+    }
+}
