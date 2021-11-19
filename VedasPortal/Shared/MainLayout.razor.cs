c83e4ff68a6c82bb8a43a5f906ef95ab7baee35b
Rukiye Asrak <rukiyeasrak@vedas.com.tr>
2021-11-19 13:55:47 +0300

    Mainlayout düzenleme yapıldı

--- VedasPortal/Shared/MainLayout.razor.cs
+++ VedasPortal/Shared/MainLayout.razor.cs
@@ -12,15 +12,18 @@ namespace VedasPortal.Shared
         protected override async void OnAfterRender(bool firstRender)
         {
             base.OnAfterRender(firstRender);
+            if (firstRender)
+            {
+                await jsRun.InvokeVoidAsync("indexInit");
+                await jsRun.InvokeVoidAsync("scriptsInit");
+                await jsRun.InvokeVoidAsync("owlCarousel");
+                await jsRun.InvokeVoidAsync("lightGallery");
+                await jsRun.InvokeVoidAsync("jssor_1_slider_init");
 
-            await jsRun.InvokeVoidAsync("indexInit");
-            await jsRun.InvokeVoidAsync("scriptsInit");
-            await jsRun.InvokeVoidAsync("owlCarousel");
-            await jsRun.InvokeVoidAsync("lightGallery");
-            await jsRun.InvokeVoidAsync("jssor_1_slider_init");
-          
+            }
 
-            
+
+
 
 
         }
