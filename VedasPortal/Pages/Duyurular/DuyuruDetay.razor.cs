9751180aa5174587408430a7cd1e4a01cbe3c91e
emrullah04 <emrullahisik@vedas.com.tr>
2022-02-02 17:43:03 +0300

    Sayfalar ilgili yerlere taşındı

---
+++ VedasPortal/Pages/Duyurular/DuyuruDetay.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using VedasPortal.Models.HaberDuyuru;
+using VedasPortal.Repository.Interface;
+
+namespace VedasPortal.Pages.Duyurular
+{
+    public partial class DuyuruDetay
+    {
+        [Inject]
+        private IBaseRepository<HaberDuyuru> DuyuruServisi { get; set; }
+
+        [Parameter]
+        public int DuyuruId { get; set; }
+
+        private HaberDuyuru DuyuruDetayGetir { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            DuyuruDetayGetir = DuyuruServisi.Get(DuyuruId);
+            return Task.CompletedTask;
+        }
+    }
+}
