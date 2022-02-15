812b8f891338682a414b8121d5d19c861341fbfc
emrullah04 <emrullahisik@vedas.com.tr>
2022-02-15 11:01:44 +0300

    Etkinlik Ekle, Katılımcı Ekle, Search Comp. Etkinlikler ve Etkinlik Detayları, Katılımcı Liste eklendi

---
+++ VedasPortal/Pages/Etkinlikler/Etkinlikler.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VedasPortal.Components.ModalComponents;
+using VedasPortal.Models.Dosya;
+using VedasPortal.Models.HaberDuyuru;
+using VedasPortal.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace VedasPortal.Pages.BasindaBiz
+{
+    public class HaberModeli : ComponentBase
+    {
+        [Inject]
+        protected IBaseRepository<HaberDuyuru> HaberServisi { get; set; }
+        protected IEnumerable<HaberDuyuru> haberler;
+
+        public Dosya HaberDosya { get; set; } = new Dosya();
+
+        protected override Task OnInitializedAsync()
+        {
+            TumHeberleriGetir();
+            return Task.CompletedTask;
+        }
+
+        protected IEnumerable<HaberDuyuru> TumHeberleriGetir()
+        {
+            haberler = HaberServisi.GetAll().AsQueryable().Include(s=>s.Dosya).ToList();
+            return haberler;
+        }
+
+        protected string DialogGorunur { get; set; } = "none";
+
+        public ModalComponent ModalDialog { get; set; }
+    }
+}
