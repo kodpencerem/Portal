3c8b120f37be145a39a1cc99f096dca0098f8eab
emrullah04 <emrullahisik@vedas.com.tr>
2022-02-17 11:59:15 +0300

    Eğitimler eklendi

---
+++ VedasPortal/Pages/FaydaliIcerikler/Egitimler/EgitimDetay.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using VedasPortal.Models.HaberDuyuru;
+using VedasPortal.Repository.Interface;
+
+namespace VedasPortal.Pages.BasindaBiz
+{
+    public partial class HaberDetay
+    {
+        [Inject]
+        private IBaseRepository<HaberDuyuru> HaberServisi { get; set; }
+
+        [Parameter]
+        public int HaberId { get; set; }
+
+        private HaberDuyuru HaberDetayGetir { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            HaberDetayGetir = HaberServisi.Get(HaberId);
+            return Task.CompletedTask;
+        }
+    }
+}
