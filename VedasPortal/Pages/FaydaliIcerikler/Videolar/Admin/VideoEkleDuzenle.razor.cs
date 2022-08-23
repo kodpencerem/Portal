663e2e8ebe33f090ee0efb816603f4f4f9600018
emrullah04 <emrullahisik@vedas.com.tr>
2022-08-23 17:47:07 +0300

    Videolar sayfası ve video ekleme sayfaları düzenlendi

---
+++ VedasPortal/Pages/FaydaliIcerikler/Videolar/Admin/VideoEkleDuzenle.razor.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VedasPortal.Components.ShowModalComponent;
+using VedasPortal.Entities.Models.Dosya;
+using VedasPortal.Entities.Models.Egitim;
+using VedasPortal.Enums;
+using VedasPortal.Repository.Interface;
+
+namespace VedasPortal.Pages.FaydaliIcerikler.Videolar.Admin
+{
+    public class VideoEklemeModeli : ComponentBase
+    {
+
+        [Inject]
+        public IBaseRepository<Dosya> VideoServisi { get; set; }
+
+        [Parameter]
+        public int VideoId { get; set; }
+
+        protected string Title = "Ekle";
+        public Dosya video = new();
+      
+        protected IEnumerable<Dosya> Videolar { get; set; }
+
+        protected IEnumerable<Dosya> TumVideolariGetir()
+        {
+            Videolar = VideoServisi.GetAll();
+            return Videolar;
+
+        }
+        public Dictionary<VideoKategori, string> Kategoriler { get; set; }
+        protected void TumKategorileriGetir()
+        {
+            var list = new Dictionary<VideoKategori, string>();
+            foreach (VideoKategori item in Enum.GetValues(typeof(VideoKategori)))
+            {
+                list.Add(item, item.TextVideo());
+            }
+            Kategoriler = list;
+        }
+
+        public Dictionary<Birimler, string> Birimler { get; set; }
+        protected void TumBirimleriGetir()
+        {
+            var list = new Dictionary<Birimler, string>();
+            foreach (Birimler item in Enum.GetValues(typeof(Birimler)))
+            {
+                list.Add(item, item.TextBirimler());
+            }
+            Birimler = list;
+        }
+
+        protected void Kayit()
+        {
+            VideoServisi.Add(video);
+
+            var fileName = SaveFileToUploaded.FileName.Split(".");
+            var filePath = SaveFileToUploaded.VideoUploadedPath;
+            var dosya = new Dosya()
+            {
+                Adi = fileName[0],
+                Yolu= filePath,
+                Uzanti = fileName[1],
+                Kategori = (DosyaKategori?)(VideoKategori)DosyaKategori.Mp4,
+                AktifPasif = true,
+                
+            };
+            VideoServisi.Add(dosya);
+        }
+        protected override void OnParametersSet()
+        {
+            if (VideoId != 0)
+            {
+                Title = "Duzenle";
+                video = VideoServisi.Get(VideoId);
+            }
+        }
+
+
+
+        protected void SilmeyiOnayla(int videoId)
+        {
+            ModalDialog.Open();
+            video = Videolar.FirstOrDefault(x => x.Id == videoId);
+        }
+        public ModalComponent ModalDialog { get; set; }
+        protected string DialogGorunur { get; set; } = "none";
+
+        protected void Sil()
+        {
+            if (video.Id == 0)
+                return;
+
+            VideoServisi.Remove(video.Id);
+            video = new Dosya();
+            TumVideolariGetir();
+            TumBirimleriGetir();
+        }
+
+
+
+        protected override Task OnInitializedAsync()
+        {
+            TumBirimleriGetir();
+            TumVideolariGetir();
+            TumKategorileriGetir();
+            return Task.CompletedTask;
+        }
+
+        public void Temizle()
+        {
+            video = null;
+        }
+
+
+        [Inject]
+        public IJSRuntime JsRun { get; set; }
+        protected override async void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            if (firstRender)
+            {
+                await JsRun.InvokeVoidAsync("dataTables");
+            }
+        }
+    }
+}
---
+++ VedasPortal/Pages/FaydaliIcerikler/Videolar/Videolar.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VedasPortal.Entities.Models.Dosya;
+using VedasPortal.Repository.Interface;
+
+namespace VedasPortal.Pages.FaydaliIcerikler.Videolar
+{
+    public class VideoModel : ComponentBase
+    {
+        [Inject]
+        protected IBaseRepository<Dosya> Video { get; set; }
+
+        protected IEnumerable<Dosya> VideoList { get; set; } = new List<Dosya>();
+        protected Dosya VideoDosya { get; set; } = new();
+
+        public string SearchText = "";
+
+        public List<Dosya> FilteredVideo => VideoList.Where(
+            x => x.Adi.ToLower().Contains(SearchText.ToLower())
+            || x.Aciklama.ToLower().Contains(SearchText.ToLower())
+            ).ToList();
+
+        protected override Task OnInitializedAsync()
+        {
+            TumVideolariGetir();
+            return Task.CompletedTask;
+        }
+
+        protected IEnumerable<Dosya> TumVideolariGetir()
+        {
+            VideoList = Video.GetAll();
+            return VideoList;
+        }
+    }
+}
